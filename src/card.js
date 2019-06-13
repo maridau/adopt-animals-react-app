ef395ae6c5b253ae4bd9c1974bc50377f48da38e
maridau <marina.dau@gmail.com>
2019-06-13 19:14:36 -0300

    nuevos css git add

---
+++ src/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  './card.css';
+
+function Card() {
+  return (
+    <div className="Card">
+      <header className="Card-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default Card;
